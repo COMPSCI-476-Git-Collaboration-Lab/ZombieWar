0862ef8ebe5f0f5c81be009df0e0575d1f78fba0
Derek Andrew Beck <derek.andrew.beck@gmail.com>
2015-10-01 14:53:01 -0500

    Shit's getting added.
    None needed.

---
+++ src/ZombieWarRunner.java
@@ -0,0 +1,9 @@
+public class ZombieWarRunner {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/zombiewar/intf/ICharacterFactory.java
@@ -0,0 +1,7 @@
+package zombiewar.intf;
+
+public interface ICharacterFactory{
+ 
+  //Make a character. If type is soldier, return a Solider object.
+  public ICharacter make(String type);
+}
