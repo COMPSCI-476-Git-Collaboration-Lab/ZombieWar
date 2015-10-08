931e5910c05b87b1dec142422f0563670d785933
Sam Kromm <krommsg12@uww.edu>
2015-10-08 15:06:49 -0500

    commented out stupid things i did, may be useful though so I left them in

--- src/ZombieWarRunner.java
+++ src/ZombieWarRunner.java
@@ -1,13 +1,22 @@
-import java.util.Random;
-
+/*import java.util.Random;
+import zombiewar.impl.*;
+import zombiewar.intf.*;*/
 public class ZombieWarRunner {
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Random randomNum= new Random();//implemented by sam
 		
+		
+		// TODO Auto-generated method stub
+		/*Random randomNum= new Random();//implemented a thing -Sam
+		CharacterFactory[] arrayOfThings= new CharacterFactory[20]; 
+		for (int i=0; i<arrayOfThings.length();)
+		
+		int zombieOrHuman = randomNum.nextInt(1);
+		
+		
+		i commented out because there was a main thing but if its acutally useful it can be uncomented -Sam */
 		
 	}
 }
